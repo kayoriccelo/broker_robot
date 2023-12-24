8809ae22ab3f71777307d7d5876bb236b15053ca
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-12-23 22:40:16 -0300

    refactor: changed the broker model fields.

--- data/config/broker/models.py
+++ data/config/broker/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from data.config.broker.choices import C_STATUS_BROKER, STATUS_BROKER_ACTIVE
+
 
 class Broker(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True, null=True, blank=True)
+    status = models.IntegerField(choices=C_STATUS_BROKER, default=STATUS_BROKER_ACTIVE, null=True, blank=True)
+    active = models.ManyToManyField('active.Active', related_name='brokers', blank=True)
 
     class Meta:
         verbose_name = u'Broker'
