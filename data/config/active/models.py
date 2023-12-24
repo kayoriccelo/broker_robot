f95eb05890b49c126cd4117438f3020ad2dd2141
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-12-23 22:41:05 -0300

    feat: create app active.

---
+++ data/config/active/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from data.config.active.choices import C_STATUS_ACTIVE, STATUS_ACTIVE_ACTIVE
+
+
+class Active(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=200)
+    status = models.IntegerField(choices=C_STATUS_ACTIVE, default=STATUS_ACTIVE_ACTIVE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = u'Active'
+        verbose_name_plural = u'Active'
+        db_table = 'active'
+
+    def __str__(self) -> str:
+        return self.description
