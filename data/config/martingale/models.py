8de98f793615ac9908775aff0a6d360157328e11
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-12-26 08:27:25 -0300

    refactor: added reverse field and str method in martingale model.

--- data/config/martingale/models.py
+++ data/config/martingale/models.py
@@ -6,10 +6,13 @@ from data.config.martingale.choices import C_STATUS_MARTINGALE, STATUS_MARTINGAL
 class Martingale(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     multiplier = models.IntegerField()
+    reverse = models.BooleanField(default=False, null=True, blank=True)
     status = models.IntegerField(choices=C_STATUS_MARTINGALE, default=STATUS_MARTINGALE_ACTIVE, null=True, blank=True)
 
     class Meta:
         verbose_name = u'Martingale'
         verbose_name_plural = u'Martingale'
         db_table = 'martingale'
-
+    
+    def __str__(self) -> str:
+        return f'Multiplier: {self.multiplier} - Reverse: {self.reverse}'
