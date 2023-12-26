5abae1a6e157709ed1eeef5cb7308b326b0190f5
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-12-26 08:29:25 -0300

    feat: create app strategy.

---
+++ data/config/strategy/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from data.config.strategy.choices import C_STATUS_STRATEGY, STATUS_STRATEGY_ACTIVE
+
+
+class Strategy(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=200)
+    status = models.IntegerField(choices=C_STATUS_STRATEGY, default=STATUS_STRATEGY_ACTIVE, null=True, blank=True)
+    
+    cycles = models.ManyToManyField('cycle.Cycle', related_name='strategies', blank=True)
+    
+    class Meta:
+        verbose_name = u'Strategy'
+        verbose_name_plural = u'Strategies'
+        db_table = 'strategy'
+
+    def __str__(self) -> str:
+        return self.description
